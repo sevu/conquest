42e805fc829e0b207441954d695e26d3713ecbb3
Severin Glöckner <severin.gloeckner@stud.htwk-leipzig.de>
2024-05-15 02:07:28 +0200

    trim down Lua code from mainline

--- lua/turns_over.lua
+++ lua/turns_over.lua
@@ -5,26 +5,15 @@
 local res = {}
 
 function res.turns_over_advantage()
-	local winning_sides, side_results = res.calc_turns_over_advantage(1)
+	local winning_sides, side_results = res.calc_turns_over_advantage()
 	wml.variables['won'] = table.concat(winning_sides, ',')
+
 	local _ = wesnoth.textdomain "wesnoth-Conquest"
 	-- po: Turn limit reached (if one was used)
 	res.show_turns_over_advantage(winning_sides, side_results, _'End of Game')
 end
 
----@class side_result
----@field income integer
----@field num_units integer
----@field gold integer
----@field total integer
-
----@alias sides_score_table table<integer, side_result|false>
-
----Calculate the turns over advantage.
----@param income_factor? integer Indicates how important income is in the calculation.
----@return integer[]
----@return sides_score_table
-function res.calc_turns_over_advantage(income_factor)
+function res.calc_turns_over_advantage()
 	local function all_sides()
 		local function f(s, i)
 			i = i + 1
@@ -34,38 +23,22 @@ function res.calc_turns_over_advantage(income_factor)
 		return f, nil, 0
 	end
 
-	income_factor = income_factor or 5
-
 	local winning_sides = {}
-	local total_score = -1
-
-	---@type sides_score_table
+	local best_score = -1
 	local side_outcomes = {}
+
 	for side, team in all_sides() do
 		if not team.__cfg.hidden then
 			if # wesnoth.units.find_on_map( { side = side } ) == 0 then
 				side_outcomes[side] = false
 			else
-				local income = team.total_income * income_factor
-				local units = 0
-				-- Calc the total unit-score here
-				for i, unit in ipairs( wesnoth.units.find_on_map { side = side } ) do
-					if not unit.__cfg.canrecruit then
-						units = units + 1
-					end
-				end
-				-- Up to here
-				local total = units + team.gold + income
-				side_outcomes[side] = {
-					income = income,
-					num_units = units,
-					gold = team.gold,
-					total = total
-				}
-				if income > total_score then
+				local income = team.total_income
+				side_outcomes[side] = {income = income}
+
+				if income > best_score then
 					winning_sides = {side}
-					total_score = income
-				elseif income == total_score then
+					best_score = income
+				elseif income == best_score then
 					table.insert(winning_sides, side)
 				end
 			end
@@ -76,24 +49,22 @@ function res.calc_turns_over_advantage(income_factor)
 end
 
 ---Show the turns over advantage popup.
----@param winning_sides integer[] The list of sides who tied for first place
----@param side_results sides_score_table The table of each side's score calculations
----@param title? tstring The title to display in the popup
 function res.show_turns_over_advantage(winning_sides, side_results, title)
-	local _ = wesnoth.textdomain "wesnoth-multiplayer"
-	---@type tstring
+	local _
 	local side_comparison = ""
 	for side = 1, #wesnoth.sides do
 		local outcome = side_results[side]
 		local side_color = wesnoth.colors[wesnoth.sides[side].color].pango_color
+
 		if outcome == false then
+			_ = wesnoth.textdomain "wesnoth-multiplayer"
 			local side_text = _ "<span strikethrough='true' foreground='$side_color'>Side $side_number</span>:  Has lost all units"
 			side_comparison = side_comparison .. side_text:vformat{side_color = side_color, side_number = side} .. "\n"
 		elseif outcome ~= nil then
 			_ = wesnoth.textdomain "wesnoth-Conquest"
 			-- po: This is a shortened string from the mainline textdomain wesnoth-multiplayer. You can copy the text from there and delete a part of it: https://gettext.wesnoth.org/?package=wesnoth-multiplayer
 			local side_text = _ "<span foreground='$side_color'>Side $side_number</span>:  Income = $income"
-			side_comparison = side_comparison .. side_text:vformat{side_color = side_color, side_number = side, income = outcome.income, units = outcome.num_units, gold = outcome.gold, total = outcome.total} .. "\n"
+			side_comparison = side_comparison .. side_text:vformat{side_color = side_color, side_number = side, income = outcome.income} .. "\n"
 		end
 	end
 
@@ -111,7 +82,7 @@ function res.show_turns_over_advantage(winning_sides, side_results, title)
 		local comparison_text = _ "Sides $winners are tied."
 		side_comparison = side_comparison .. "\n" .. comparison_text:vformat{winners = winners}
 	end
-	-- if #winning_sides==0, then every side either has no units or has a negative score
+
 	title = title or _ "dialog^Turns Over"
 	gui.show_popup(title, side_comparison)
 end
