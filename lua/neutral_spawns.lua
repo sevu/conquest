a9735ffc77675dd90ed7c52fff3d6c0ebee75a19
Severin Glöckner <severin.gloeckner@stud.htwk-leipzig.de>
2024-06-23 23:43:32 +0200

    make it easier to change region code

--- lua/neutral_spawns.lua
+++ lua/neutral_spawns.lua
@@ -46,174 +46,194 @@ if (spawns_theme == 1) or (spawns_theme == 2) or (spawns_theme == 7) or (spawns_
 			wesnoth.game_events.fire(mathx.random_choice('ce_spawn_5g_Cavalry,ce_spawn_5g_Cavalry,ce_spawn_5g_Cavalry,ce_spawn_4g_Dwarvishstalwart,ce_spawn_4g_Dwarvishstalwart,ce_spawn_4g_Dwarvishstalwart,ce_spawn_3g_Sergeant,ce_spawn_3g_Sergeant,ce_spawn_2g_Dwarvishguardsman,ce_spawn_1g_militia'))
 
 		end
+
 		wml.variables.ce_spawn = nil
 	end
 
 ---------------------------------------------------------------
-elseif spawns_theme == 3 then
--- Balanced Hard
--- Region aware code, places one strong unit into every region and many weaker ones.
----------------------------------------------------------------
-	local lua_total_regions = wml.variables['CE_SYSTEM.regions.length']
-	for i=0,lua_total_regions-1,1 do
-		local lua_current_region = wml.variables['CE_SYSTEM.regions['..i..'].id']
-		local lua_total_villages = wml.variables['CE_SYSTEM.regions_'..lua_current_region..'.length']
-		local counter = lua_total_villages
+elseif (spawns_theme == 3) or (spawns_theme == 6) then
 
-		-- In difference to the previous, this algorithm is not random but deterministic.
-		-- Loop over all villages, but in random order.
-		-- That way the units placed in a region are deterministic, but their position in the region is random.
-		local order = {}
-		for j=0,lua_total_villages-1,1 do
-			table.insert(order, j)
+
+	-- Balanced Hard
+	-- Region aware code, places one strong unit into every region and many weaker ones.
+	---------------------------------------------------------------
+	local function spawn_hard(lua_total_villages, counter)
+		if lua_total_villages == 1 then
+			wesnoth.game_events.fire(mathx.random_choice('ce_spawn_5g_Pikeman,ce_spawn_5g_Cavalry'))
 		end
-		mathx.shuffle(order)
 
-		for z,j in ipairs(order) do
-			local spawn_x = wml.variables['CE_SYSTEM.regions_'..lua_current_region..'['..j..'].x']
-			local spawn_y = wml.variables['CE_SYSTEM.regions_'..lua_current_region..'['..j..'].y']
-			local neutral_village = #wesnoth.map.find{ owner_side = 0, gives_income = true, x = spawn_x, y = spawn_y } > 0
+		-- 2 villages regions have one L3 and L5
+		if lua_total_villages == 2 then
+			if counter == 0 then
+				wesnoth.game_events.fire('ce_spawn_3g_Sergeant')
+			else
+				wesnoth.game_events.fire(mathx.random_choice('ce_spawn_5g_Pikeman,ce_spawn_5g_Cavalry'))
+			end
+		end
 
-			if neutral_village then
-				-- The counter and the different cases ensure that not if some villages are occupied by plyers,
-				-- then the weak units are skipped and not the strong ones.
-				counter = counter - 1
+		-- 3 villages regions have one L1, L3 and L5
+		if lua_total_villages == 3 then
+			if counter == 0 then
+				wesnoth.game_events.fire('ce_spawn_1g_militia')
+			elseif counter == 1 then
+				wesnoth.game_events.fire('ce_spawn_3g_Sergeant')
+			else
+				wesnoth.game_events.fire(mathx.random_choice('ce_spawn_5g_Pikeman,ce_spawn_5g_Cavalry'))
+			end
+		end
 
-				wml.variables.ce_spawn = { side = 7, x = spawn_x, y = spawn_y }
+		-- 4 villages regions have one L1, two L3 and one L5
+		-- This is a second L3 instead of a second L5 compared to Initial
+		if lua_total_villages == 4 then
+			if counter == 0 then
+				wesnoth.game_events.fire('ce_spawn_1g_militia')
+			elseif counter == 1 then
+				wesnoth.game_events.fire('ce_spawn_3g_Sergeant')
+			elseif counter == 2 then
+				wesnoth.game_events.fire('ce_spawn_3g_Sergeant')
+			else
+				wesnoth.game_events.fire(mathx.random_choice('ce_spawn_5g_Pikeman,ce_spawn_5g_Cavalry'))
+			end
+		end
 
-				if lua_total_villages == 1 then
-					wesnoth.game_events.fire(mathx.random_choice('ce_spawn_5g_Pikeman,ce_spawn_5g_Cavalry'))
-				end
+		-- 5 villages regions have two L1, two L3 and one L8
+		-- Compared to Initial it has an L3 instead of an L5
+		if lua_total_villages == 5 then
+			if counter == 0 then
+				wesnoth.game_events.fire('ce_spawn_1g_militia')
+			elseif counter == 1 then
+				wesnoth.game_events.fire('ce_spawn_1g_militia')
+			elseif counter == 2 then
+				wesnoth.game_events.fire('ce_spawn_3g_Sergeant')
+			elseif counter == 3 then
+				wesnoth.game_events.fire('ce_spawn_3g_Sergeant')
+			else
+				wesnoth.game_events.fire('ce_spawn_8g_Eliteinfantry')
+			end
+		end
 
-				-- 2 villages regions have one L3 and L5
-				if lua_total_villages == 2 then
-					if counter == 0 then
-						wesnoth.game_events.fire('ce_spawn_3g_Sergeant')
-					else
-						wesnoth.game_events.fire(mathx.random_choice('ce_spawn_5g_Pikeman,ce_spawn_5g_Cavalry'))
-					end
-				end
+		-- 6 villages regions have three L1, one L3, one L5 and one L8
+		-- Initial has an L10 an and L5 instead of two L1
+		if lua_total_villages == 6 then
+			if counter == 0 then
+				wesnoth.game_events.fire('ce_spawn_1g_militia')
+			elseif counter == 1 then
+				wesnoth.game_events.fire('ce_spawn_1g_militia')
+			elseif counter == 2 then
+				wesnoth.game_events.fire('ce_spawn_1g_militia')
+			elseif counter == 3 then
+				wesnoth.game_events.fire('ce_spawn_3g_Sergeant')
+			elseif counter == 4 then
+				wesnoth.game_events.fire(mathx.random_choice('ce_spawn_5g_Pikeman,ce_spawn_5g_Cavalry'))
+			else
+				wesnoth.game_events.fire('ce_spawn_8g_Eliteinfantry')
+			end
+		end
 
-				-- 3 villages regions have one L1, L3 and L5
-				if lua_total_villages == 3 then
-					if counter == 0 then
-						wesnoth.game_events.fire('ce_spawn_1g_militia')
-					elseif counter == 1 then
-						wesnoth.game_events.fire('ce_spawn_3g_Sergeant')
-					else
-						wesnoth.game_events.fire(mathx.random_choice('ce_spawn_5g_Pikeman,ce_spawn_5g_Cavalry'))
-					end
-				end
+		-- 7 villages regions have five L1, one L3 and one L15
+		-- Initial has an L10, L8, two L5 instead of four L1
+		if lua_total_villages == 7 then
+			if counter == 0 then
+				wesnoth.game_events.fire('ce_spawn_1g_militia')
+			elseif counter == 1 then
+				wesnoth.game_events.fire('ce_spawn_1g_militia')
+			elseif counter == 2 then
+				wesnoth.game_events.fire('ce_spawn_1g_militia')
+			elseif counter == 3 then
+				wesnoth.game_events.fire('ce_spawn_1g_militia')
+			elseif counter == 4 then
+				wesnoth.game_events.fire('ce_spawn_1g_militia')
+			elseif counter == 5 then
+				wesnoth.game_events.fire('ce_spawn_3g_Sergeant')
+			else
+				wesnoth.game_events.fire('ce_spawn_15g_Lieutenant')
+			end
+		end
 
-				-- 4 villages regions have one L1, two L3 and one L5
-				-- This is a second L3 instead of a second L5 compared to Initial
-				if lua_total_villages == 4 then
-					if counter == 0 then
-						wesnoth.game_events.fire('ce_spawn_1g_militia')
-					elseif counter == 1 then
-						wesnoth.game_events.fire('ce_spawn_3g_Sergeant')
-					elseif counter == 2 then
-						wesnoth.game_events.fire('ce_spawn_3g_Sergeant')
-					else
-						wesnoth.game_events.fire(mathx.random_choice('ce_spawn_5g_Pikeman,ce_spawn_5g_Cavalry'))
-					end
-				end
-
-				-- 5 villages regions have two L1, two L3 and one L8
-				-- Compared to Initial it has an L3 instead of an L5
-				if lua_total_villages == 5 then
-					if counter == 0 then
-						wesnoth.game_events.fire('ce_spawn_1g_militia')
-					elseif counter == 1 then
-						wesnoth.game_events.fire('ce_spawn_1g_militia')
-					elseif counter == 2 then
-						wesnoth.game_events.fire('ce_spawn_3g_Sergeant')
-					elseif counter == 3 then
-						wesnoth.game_events.fire('ce_spawn_3g_Sergeant')
-					else
-						wesnoth.game_events.fire('ce_spawn_8g_Eliteinfantry')
-					end
-				end
-
-				-- 6 villages regions have three L1, one L3, one L5 and one L8
-				-- Initial has an L10 an and L5 instead of two L1
-				if lua_total_villages == 6 then
-					if counter == 0 then
-						wesnoth.game_events.fire('ce_spawn_1g_militia')
-					elseif counter == 1 then
-						wesnoth.game_events.fire('ce_spawn_1g_militia')
-					elseif counter == 2 then
-						wesnoth.game_events.fire('ce_spawn_1g_militia')
-					elseif counter == 3 then
-						wesnoth.game_events.fire('ce_spawn_3g_Sergeant')
-					elseif counter == 4 then
-						wesnoth.game_events.fire(mathx.random_choice('ce_spawn_5g_Pikeman,ce_spawn_5g_Cavalry'))
-					else
-						wesnoth.game_events.fire('ce_spawn_8g_Eliteinfantry')
-					end
-				end
-
-				-- 7 villages regions have five L1, one L3 and one L15
-				-- Initial has an L10, L8, two L5 instead of four L1
-				if lua_total_villages == 7 then
-					if counter == 0 then
-						wesnoth.game_events.fire('ce_spawn_1g_militia')
-					elseif counter == 1 then
-						wesnoth.game_events.fire('ce_spawn_1g_militia')
-					elseif counter == 2 then
-						wesnoth.game_events.fire('ce_spawn_1g_militia')
-					elseif counter == 3 then
-						wesnoth.game_events.fire('ce_spawn_1g_militia')
-					elseif counter == 4 then
-						wesnoth.game_events.fire('ce_spawn_1g_militia')
-					elseif counter == 5 then
-						wesnoth.game_events.fire('ce_spawn_3g_Sergeant')
-					else
-						wesnoth.game_events.fire('ce_spawn_15g_Lieutenant')
-					end
-				end
-
-				-- Even bigger regions have five L1, one L3 and one L15 and for each additional village an L5
-				-- Unlike on Initial, each additional village receives an L5 instead of L15
-				if lua_total_villages > 7 then
-					if counter == 0 then
-						wesnoth.game_events.fire('ce_spawn_1g_militia')
-					elseif counter == 1 then
-						wesnoth.game_events.fire('ce_spawn_1g_militia')
-					elseif counter == 2 then
-						wesnoth.game_events.fire('ce_spawn_1g_militia')
-					elseif counter == 3 then
-						wesnoth.game_events.fire('ce_spawn_1g_militia')
-					elseif counter == 4 then
-						wesnoth.game_events.fire('ce_spawn_1g_militia')
-					elseif counter == 5 then
-						wesnoth.game_events.fire('ce_spawn_3g_Sergeant')
-					elseif counter == 6 then
-						wesnoth.game_events.fire('ce_spawn_15g_Lieutenant')
-					else
-						wesnoth.game_events.fire(mathx.random_choice('ce_spawn_5g_Pikeman,ce_spawn_5g_Cavalry'))
-					end
-				end
-
-				wml.variables.ce_spawn = nil
+		-- Even bigger regions have five L1, one L3 and one L15 and for each additional village an L5
+		-- Unlike on Initial, each additional village receives an L5 instead of L15
+		if lua_total_villages > 7 then
+			if counter == 0 then
+				wesnoth.game_events.fire('ce_spawn_1g_militia')
+			elseif counter == 1 then
+				wesnoth.game_events.fire('ce_spawn_1g_militia')
+			elseif counter == 2 then
+				wesnoth.game_events.fire('ce_spawn_1g_militia')
+			elseif counter == 3 then
+				wesnoth.game_events.fire('ce_spawn_1g_militia')
+			elseif counter == 4 then
+				wesnoth.game_events.fire('ce_spawn_1g_militia')
+			elseif counter == 5 then
+				wesnoth.game_events.fire('ce_spawn_3g_Sergeant')
+			elseif counter == 6 then
+				wesnoth.game_events.fire('ce_spawn_15g_Lieutenant')
+			else
+				wesnoth.game_events.fire(mathx.random_choice('ce_spawn_5g_Pikeman,ce_spawn_5g_Cavalry'))
 			end
 		end
 	end
 
----------------------------------------------------------------
-elseif spawns_theme == 6 then
--- Hard Initial
--- Region aware code, places in bigger regions stronger units.
----------------------------------------------------------------
--- For all regions.
+
+	-- Hard Initial
+	-- Region aware code, places in bigger regions stronger units.
+	---------------------------------------------------------------
+	local function spawn_initial(lua_total_villages, counter)
+		-- Custom for 1 village regions.
+		if lua_total_villages == 1 then
+			wesnoth.game_events.fire(mathx.random_choice('ce_spawn_5g_Pikeman,ce_spawn_5g_Cavalry'))
+		end
+
+		-- Custom for 2 village regions.
+		if lua_total_villages == 2 then
+			if counter == 0 then
+				wesnoth.game_events.fire('ce_spawn_3g_Sergeant')
+			else
+				wesnoth.game_events.fire(mathx.random_choice('ce_spawn_5g_Pikeman,ce_spawn_5g_Cavalry'))
+			end
+		end
+
+		-- Algorithm for big regions.
+		-- The 2 themes differ only in this point.
+		-- 4 villages: L1, L3, two L5
+		-- 5 villages: L1, L3, two L5, L8
+		-- 6 villages: L1, L3, two L5, L8, L10
+		-- 7 villages: L1, L3, two L5, L8, L10, L15 for each additional village
+
+		if lua_total_villages > 2 then
+			if counter == 0 then
+				wesnoth.game_events.fire('ce_spawn_1g_militia')
+			elseif counter == 1 then
+				wesnoth.game_events.fire('ce_spawn_3g_Sergeant')
+			elseif counter == 2 then
+				wesnoth.game_events.fire(mathx.random_choice('ce_spawn_5g_Pikeman,ce_spawn_5g_Cavalry'))
+			elseif counter == 3 then
+				wesnoth.game_events.fire(mathx.random_choice('ce_spawn_5g_Pikeman,ce_spawn_5g_Cavalry'))
+			elseif counter == 4 then
+				wesnoth.game_events.fire('ce_spawn_8g_Eliteinfantry')
+			elseif counter == 5 then
+				wesnoth.game_events.fire('ce_spawn_10g_Lancer')
+			elseif counter == 6 then
+				wesnoth.game_events.fire('ce_spawn_15g_Lieutenant')
+			else
+				-- Exists only on Pasarganta maps.
+				wesnoth.game_events.fire(mathx.random_choice('ce_spawn_5g_Pikeman,ce_spawn_8g_Eliteinfantry,ce_spawn_10g_Lancer'))
+			end
+		end
+	end
+
+
+	---------------------------------------------------------------
+	-- In difference to the previous, this algorithm is not random but deterministic.
+	-- For all regions.
 	local lua_total_regions = wml.variables['CE_SYSTEM.regions.length']
 	for i=0,lua_total_regions-1,1 do
 		local lua_current_region = wml.variables['CE_SYSTEM.regions['..i..'].id']
 		local lua_total_villages = wml.variables['CE_SYSTEM.regions_'..lua_current_region..'.length']
 		local counter = lua_total_villages
 
+		-- Loop over all villages, but in random order.
+		-- That way the units placed in a region are deterministic, but their position in the region is random.
 		local order = {}
-		-- For all villages of this region.
 		for j=0,lua_total_villages-1,1 do
 			table.insert(order, j)
 		end
@@ -226,51 +246,18 @@ elseif spawns_theme == 6 then
 			local neutral_village = #wesnoth.map.find{ owner_side = 0, gives_income = true, x = spawn_x, y = spawn_y } > 0
 
 			if neutral_village then
+				-- The counter and the different cases ensure that if some villages are occupied by plyers,
+				-- then the weak units are skipped and not the strong ones.
+				--
 				-- For regions where players have some units, placing of the weak units is skipped.
 				counter = counter - 1
 
 				wml.variables.ce_spawn = { side = 7, x = spawn_x, y = spawn_y }
 
-				-- Custom for 1 village regions.
-				if lua_total_villages == 1 then
-					wesnoth.game_events.fire(mathx.random_choice('ce_spawn_5g_Pikeman,ce_spawn_5g_Cavalry'))
-				end
-
-				-- Custom for 2 village regions.
-				if lua_total_villages == 2 then
-					if counter == 0 then
-						wesnoth.game_events.fire('ce_spawn_3g_Sergeant')
-					else
-						wesnoth.game_events.fire(mathx.random_choice('ce_spawn_5g_Pikeman,ce_spawn_5g_Cavalry'))
-					end
-				end
-
-				-- Algorithm for big regions.
-				-- The 2 themes differ only in this point.
-				-- 4 villages: L1, L3, two L5
-				-- 5 villages: L1, L3, two L5, L8
-				-- 6 villages: L1, L3, two L5, L8, L10
-				-- 7 villages: L1, L3, two L5, L8, L10, L15 for each additional village
-
-				if lua_total_villages > 2 then
-					if counter == 0 then
-						wesnoth.game_events.fire('ce_spawn_1g_militia')
-					elseif counter == 1 then
-						wesnoth.game_events.fire('ce_spawn_3g_Sergeant')
-					elseif counter == 2 then
-						wesnoth.game_events.fire(mathx.random_choice('ce_spawn_5g_Pikeman,ce_spawn_5g_Cavalry'))
-					elseif counter == 3 then
-						wesnoth.game_events.fire(mathx.random_choice('ce_spawn_5g_Pikeman,ce_spawn_5g_Cavalry'))
-					elseif counter == 4 then
-						wesnoth.game_events.fire('ce_spawn_8g_Eliteinfantry')
-					elseif counter == 5 then
-						wesnoth.game_events.fire('ce_spawn_10g_Lancer')
-					elseif counter == 6 then
-						wesnoth.game_events.fire('ce_spawn_15g_Lieutenant')
-					else
-						-- Exists only on Pasarganta maps.
-						wesnoth.game_events.fire(mathx.random_choice('ce_spawn_5g_Pikeman,ce_spawn_8g_Eliteinfantry,ce_spawn_10g_Lancer'))
-					end
+				if spawns_theme == 3 then
+					spawn_hard(lua_total_villages, counter)
+				elseif spawns_theme == 6 then
+					spawn_initial(lua_total_villages, counter)
 				end
 
 				wml.variables.ce_spawn = nil
