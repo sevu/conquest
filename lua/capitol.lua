b1225a9c651690cae86edaa513ba27370048432f
Severin Glöckner <severin.gloeckner@stud.htwk-leipzig.de>
2024-06-24 16:06:44 +0200

    rename lua variables

--- lua/capitol.lua
+++ lua/capitol.lua
@@ -1,25 +1,25 @@
 -- << Magic marker. For Lua it's a comment, for the WML preprocessor an opening quotation sign.
 
 local _ = wesnoth.textdomain 'wesnoth-Conquest'
-local lua_all_villages = wesnoth.map.find{ gives_income = true }
-local lua_total_villages = #lua_all_villages - 1
-local lua_number_of_attempts = wml.variables['CE_SYSTEM.number_of_attempts'] or 1
-local lua_friendly_distance = wml.variables['CE_SYSTEM.max_distance'] or 8
-local lua_enemy_distance = wml.variables['CE_SYSTEM.min_distance'] or 12
+local all_villages = wesnoth.map.find{ gives_income = true }
+local total_villages = #all_villages - 1
+local number_of_attempts = wml.variables['CE_SYSTEM.number_of_attempts'] or 1
+local friendly_distance = wml.variables['CE_SYSTEM.max_distance'] or 8
+local enemy_distance = wml.variables['CE_SYSTEM.min_distance'] or 12
 
 
 -- Loop to retry with lower distance to other players.
-for d=lua_enemy_distance,5,-1 do
+for d=enemy_distance,5,-1 do
 	wesnoth.interface.delay(1)
 	wesnoth.interface.add_chat_message('Conquest',stringx.vformat(_'Distance $d', {d=d}))
 
 	-- Loop to retry with same settings.
-	for k=1,lua_number_of_attempts,1 do
+	for k=1,number_of_attempts,1 do
 		local text = _'Attempt $number out of $max'
 		wesnoth.interface.delay(1)
-		wesnoth.interface.add_chat_message('Conquest',text:vformat({ number=k, max=lua_number_of_attempts }))
+		wesnoth.interface.add_chat_message('Conquest',text:vformat({ number=k, max=number_of_attempts }))
 
-		local random_first_villa = mathx.random(0, lua_total_villages)
+		local random_first_villa = mathx.random(0, total_villages)
 		local all_sides = wesnoth.sides.find{ wml.tag.has_unit { canrecruit = true } }
 
 		for sides_counter,s in ipairs(all_sides) do
@@ -32,7 +32,7 @@ for d=lua_enemy_distance,5,-1 do
 
 				-- Determine a random village.
 				local counter = 0
-				for i, pairs_xy in ipairs(lua_all_villages) do
+				for i, pairs_xy in ipairs(all_villages) do
 					if random_first_villa == counter then
 						wml.variables.ce_spawn = { side = current_side, x = pairs_xy.x, y = pairs_xy.y }
 						wesnoth.game_events.fire('ce_spawn_1g_militia')
@@ -44,7 +44,7 @@ for d=lua_enemy_distance,5,-1 do
 				--- for first side store nearby villages in settings radius
 				--- and place 2 militia in randomly shuffled 2 of them
 				local all_friendly_villages = wesnoth.map.find{ owner_side = 0, gives_income = true, {'and',
-					{ owner_side=current_side, gives_income = true, radius=lua_friendly_distance }}}
+					{ owner_side=current_side, gives_income = true, radius=friendly_distance }}}
 
 				-- Choose 2nd and 3rd village village.
 				if #all_friendly_villages > 1 then
@@ -71,22 +71,22 @@ for d=lua_enemy_distance,5,-1 do
 			-- If it is not the first side.
 			else
 				---try if you can put next side with specified distances..
-				local lua_all_villages_left = wesnoth.map.find{ owner_side=0, gives_income = true, {'and',
+				local all_villages_left = wesnoth.map.find{ owner_side=0, gives_income = true, {'and',
 					{{'not', { gives_income = true, radius=d, {'not', { owner_side=0 }} }} }} }
 
-				local lua_total_villages_left = #lua_all_villages_left - 1
+				local total_villages_left = #all_villages_left - 1
 
 				-- Loop with up to 10 tries.
 				for n=1,10,1 do
 					if break_random_villa_cycle == false then
 
 						-- Safety check for random function.
-						if lua_total_villages_left >= 0 then
-							local random_villa = mathx.random(0, lua_total_villages_left)
+						if total_villages_left >= 0 then
+							local random_villa = mathx.random(0, total_villages_left)
 							local counter = 0
 
 							-- Spawn 1 village.
-							for i, pairs_xy in ipairs(lua_all_villages_left) do
+							for i, pairs_xy in ipairs(all_villages_left) do
 								if random_villa == counter then
 									wml.variables.ce_spawn = { side = current_side, x = pairs_xy.x, y = pairs_xy.y }
 									wesnoth.game_events.fire('ce_spawn_1g_militia')
@@ -104,7 +104,7 @@ for d=lua_enemy_distance,5,-1 do
 						local all_friendly_villages = wesnoth.map.find{ gives_income = true, owner_side=0, {'and',
 							{{'not', { gives_income = true, radius=d, {'not',{ owner_side=0 }},
 							{'and', {{'not', { owner_side=current_side}} }} }} }},
-							{'and', { gives_income = true, radius=lua_friendly_distance, owner_side=current_side }} }
+							{'and', { gives_income = true, radius=friendly_distance, owner_side=current_side }} }
 
 						-- Place next 2 villages for the same side.
 						if #all_friendly_villages > 1 then
@@ -159,6 +159,6 @@ for d=lua_enemy_distance,5,-1 do
 end
 
 
-wesnoth.interface.add_chat_message('Conquest',stringx.vformat(_'Failed to alocate starting postions for all sides! Restart the game. For random maps, it helps to use a bigger map or to increase the number of attempts. Distance to own villages was $max|.', { max = lua_friendly_distance } ))
+wesnoth.interface.add_chat_message('Conquest',stringx.vformat(_'Failed to alocate starting postions for all sides! Restart the game. For random maps, it helps to use a bigger map or to increase the number of attempts. Distance to own villages was $max|.', { max = friendly_distance } ))
 
 -- Magic marker. For Lua it's a comment, for the WML preprocessor a closing quotation sign. >>
