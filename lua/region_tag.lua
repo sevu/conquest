c3e388530cac5b9fb88245783f75e5c4f59f3524
Severin Glöckner <severin.gloeckner@stud.htwk-leipzig.de>
2025-02-15 18:21:55 +0100

    support a region table

--- lua/region_tag.lua
+++ lua/region_tag.lua
@@ -4,6 +4,7 @@
 -- Unused data is commented with --
 
 function wesnoth.wml_actions.region(cfg)
+	local t = wesnoth.textdomain 'wesnoth-Conquest_Vilas'
 	local village_list = tostring(cfg.village_list) or wml.error '[region] expects a village_list= attribute.'
 	local region_name  = tostring(cfg.name) or wml.error '[region] expects a name= attribute.'
 	local region_bonus = cfg.bonus or wml.error '[region] expects a bonus= attribute.'
@@ -46,13 +47,33 @@ function wesnoth.wml_actions.region(cfg)
 		wml.variables['CE_SYSTEM.regions['..array_num..'].id'] = region_codename
 		wml.variables['CE_SYSTEM.regions['..array_num..'].bonus'] = region_bonus
 		-- wml.variables['CE_SYSTEM.regions['..array_num..'].name'] = region_name
+
+		-- Special mode for Dative map,
+		-- place a table with an extra label describing the bonus of the region.
+		if wml.variables['CE_SYSTEM.region_table'] then
+			local tab_x = stringx.split(wml.variables['CE_SYSTEM.region_table'])[1] + (wml.variables['CE_SYSTEM.column'] or 0)
+			local tab_y = stringx.split(wml.variables['CE_SYSTEM.region_table'])[2] + (wml.variables['CE_SYSTEM.row'] or 0)
+			local bonus = region_bonus .. ' ' .. wesnoth.textdomain 'wesnoth' 'Gold'
+			local n, ne, se, s, sw, nw = wesnoth.map.get_adjacent_hexes(tab_x, tab_y)
+
+			wesnoth.map.add_label { text = t(region_name), color = region_color, x = tab_x, y = tab_y, visible_in_shroud = true }
+			wesnoth.map.add_label { text = bonus, color = region_color, x = se.x, y = se.y, visible_in_shroud = true }
+
+			-- Determine next coordinates.
+			if ((wml.variables['CE_SYSTEM.row'] or 0 ) + 1 ) < (wml.variables['CE_SYSTEM.rows'] or 4) then
+				wml.variables['CE_SYSTEM.row'] = (wml.variables['CE_SYSTEM.row'] or 0) + 1
+			else
+				wml.variables['CE_SYSTEM.row'] = 0
+				wml.variables['CE_SYSTEM.column'] = (wml.variables['CE_SYSTEM.column'] or 0) + 4
+			end
+		end
 	end
 
 
-	local previous, village_x, village_y, village_name
+	local previous, village_x, village_y, village_name, village_text
 	local offset_x = wml.variables['CE_SYSTEM.offset_x'] or 0
 	local offset_y = wml.variables['CE_SYSTEM.offset_y'] or 0
-	local t = wesnoth.textdomain 'wesnoth-Conquest_Vilas'
+	local short_label = wml.variables['CE_SYSTEM.short_label']
 
 	for eachword in string.gmatch(village_list, '([^,]+)') do
 		if tonumber(eachword) ~= nil then
@@ -81,8 +102,14 @@ function wesnoth.wml_actions.region(cfg)
 			-- wml.variables['CE_SYSTEM.regions_city_'..village_x..'_'..village_y..'.region_id'] = region_codename
 			-- wml.variables['CE_SYSTEM.regions_city_'..village_x..'_'..village_y..'.array_num'] = array_num
 
+			if short_label then
+				village_text = t(village_name)..' ('..t(region_name)..')'
+			else
+				village_text = t(village_name)..' ('..t(region_name)..' +'..region_bonus..')'
+			end
+
 			wesnoth.map.add_label {
-				text = t(village_name)..' ('..t(region_name)..' +'..region_bonus..')',
+				text = village_text,
 				color = region_color,
 				x = village_x,
 				y = village_y,
